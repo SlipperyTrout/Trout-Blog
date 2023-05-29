d90a70894947d62df7cfbd2029224e59129fb577
Milan <m.rasche@student.fontys.nl>
2023-05-29 13:23:04 +0200

    Created the Article Page that gets the article for a given id from the database

---
+++ TroutBlog/TroutBlog/Data/AppDBContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TroutBlog.Data.Entities;
+
+namespace TroutBlog.Data
+{
+    public class AppDBContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public AppDBContext(IConfiguration configuration) 
+        {
+            this.Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(Configuration.GetConnectionString("DatabaseConnection"));
+        }
+
+        public DbSet<BlogPost> Posts { get; set; }
+    }
+}
---
+++ TroutBlog/TroutBlog/Data/Entities/BlogPost.cs
@@ -0,0 +1,11 @@
+﻿namespace TroutBlog.Data.Entities
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+        public DateTime PostCreationTime { get; set; }
+        public string Title { get; set; }
+        public string Introduction { get; set; }
+        public string Body { get; set; }
+    }
+}
