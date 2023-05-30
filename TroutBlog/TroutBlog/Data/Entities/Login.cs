4b0336ec8b36bafb407d909c4c2a3ef2fb42585f
Milan Rasche <theslipperytrout@gmail.com>
2023-05-30 22:15:59 +0200

    Started work on the login class. Got the basic layout down still needs implementation

---
+++ TroutBlog/TroutBlog/Data/Entities/Login.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace TroutBlog.Data.Entities
+{
+    public class UserLogin
+    {
+        [Required]
+        public string? Email { get; set; }
+        [Required]
+        public string? Password { get; set; }
+
+    }
+}
