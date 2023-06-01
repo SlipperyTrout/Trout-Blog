12442fe12c8356c7d816638bb0dbb072c5788204
Milan Rasche <theslipperytrout@gmail.com>
2023-06-01 18:00:28 +0200

    Got the validator for the text boxes working as intended

---
+++ TroutBlog/TroutBlog/Data/Entities/ConnectedUser.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TroutBlog.Data.Entities
+{
+    public class ConnectedUser
+    {
+        [Required]
+        [EmailAddress, StringLength(32,MinimumLength = 4, ErrorMessage="{0} length must be between {2} and {1} characters")]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
--- TroutBlog/TroutBlog/Data/Entities/User.cs
+++ TroutBlog/TroutBlog/Data/Entities/User.cs
@@ -1,12 +1,21 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Components;
+using System.ComponentModel.DataAnnotations;
+using System.Linq.Expressions;
 
 namespace TroutBlog.Data.Entities
 {
     public class User
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(32,MinimumLength = 4, ErrorMessage="{0} length must be between {2} and {1} characters")]
         public string Username { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [StringLength(64, MinimumLength = 8, ErrorMessage = "{0} length must be between {2} and {1} characters")]
         public string Password { get; set; }
     }
+
 }
